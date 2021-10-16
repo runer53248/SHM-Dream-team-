0f22296bd9d1dd0a393fb9398642fa83a9867c2f
Piotr Rogalski <progalski89@gmail.com>
2021-10-16 15:03:28 +0200

    Added test case when Coordinates are not equal

--- test/CoordinatesTest.cpp
+++ test/CoordinatesTest.cpp
@@ -1,8 +1,16 @@
 #include <gtest/gtest.h>
 #include "game/Coordinates.hpp"
 
-TEST(CoordinatesTest, Basic) {
+TEST(CoordinatesTest, ShouldBeEqual) {
     constexpr Coordinates first((Coordinates::Coord<Axis::X>(10)), (Coordinates::Coord<Axis::Y>(20)));
     constexpr Coordinates second((Coordinates::Coord<Axis::X>(10)), (Coordinates::Coord<Axis::Y>(20)));
     ASSERT_EQ(first, second);
+    constexpr Coordinates third((Coordinates::Coord<Axis::X>(0)), (Coordinates::Coord<Axis::Y>(0)));
+    ASSERT_EQ(first == third, false);
+}
+
+TEST(CoordinatesTest, ShouldNotBeEqual) {
+    constexpr Coordinates first((Coordinates::Coord<Axis::X>(10)), (Coordinates::Coord<Axis::Y>(20)));
+    constexpr Coordinates second((Coordinates::Coord<Axis::X>(0)), (Coordinates::Coord<Axis::Y>(0)));
+    ASSERT_EQ(first == second, false);
 }
