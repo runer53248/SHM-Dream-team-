d315d900ffea29df500f81c6e9641bd51370b5e3
Runer <runer53248@gmail.com>
2021-10-16 15:05:21 +0200

    Change example for json.

--- utility/json/example/jsonMain.cpp
+++ utility/json/example/jsonMain.cpp
@@ -1,101 +1,75 @@
 #include "../JsonSerializer.hpp"
 
-class ExampleObject {
+struct Span {
+    int min = 0;
+    int current = 1;
+    int max = 10;
+};
+
+class Ship {
 public:
-    ExampleObject(int x, int y, std::string name)
-        : x(x), y(y), name(name) {}
+    std::string name = "Default";
+    int speed = 0;
+    int durability = 0;
+    int hp = 100;
+    struct Crew {
+        Span number;
+    };
 
-    void print() {
-        std::cout << "x = " << x << '\n';
-        std::cout << "y = " << y << '\n';
-        std::cout << "name = " << name << "\n\n";
-    }
-
-    int x;
-    int y;
-    std::string name;
+    Crew crew;
 };
 
 template <>
-Json JsonConvert::to_json(const std::unique_ptr<ExampleObject>& object) {
-    static int id = 0;
-
+Json JsonConvert::to_json(const std::unique_ptr<Ship>& object) {
     Json json;
-
-    json.putValue("ID", id++);
-    json.putValue("x", object->x);
-    json.putValue("y", object->y);
     json.putValue("name", object->name);
-    
+    json.putValue("speed", object->speed);
+    json.putValue("durability", object->durability);
+    json.putValue("hp", object->hp);
+
+    json.putBlockValue("Crew", "min", object->crew.number.min);
+    json.putBlockValue("Crew", "current", object->crew.number.current);
+    json.putBlockValue("Crew", "max", object->crew.number.max);
+
     return json;
 }
 
 template <>
-std::unique_ptr<ExampleObject> JsonConvert::from_json(const Json& json) {
-    std::cout << "Loaded object with id: " << json.getValue<size_t>("ID") << '\n';
-    auto x = json.getValue<int>("x");
-    auto y = json.getValue<int>("y");
-    auto name = json.getValue<std::string>("name");
-    return std::make_unique<ExampleObject>(x, y, name);
+std::unique_ptr<Ship> JsonConvert::from_json(const Json& json) {
+    const auto name = json.getValue<std::string>("name");
+    const auto speed = json.getValue<int>("speed");
+    const auto durability = json.getValue<int>("durability");
+    const auto hp = json.getValue<int>("hp");
+
+    const auto min = json.getBlockValue<int>("Crew", "min");
+    const auto current = json.getBlockValue<int>("Crew", "current");
+    const auto max = json.getBlockValue<int>("Crew", "max");
+
+    return std::make_unique<Ship>(Ship{
+        .name = name,
+        .speed = speed,
+        .durability = durability,
+        .hp = hp,
+        .crew = {min, current, max}});
 }
 
 int main() {
-    std::array<std::unique_ptr<ExampleObject>, 3> objects = {
-        std::make_unique<ExampleObject>(5, 10, "Object 1"),
-        std::make_unique<ExampleObject>(15, 110, "Object 2"),
-        std::make_unique<ExampleObject>(25, 1110, "Object 3"),
-    };
+    auto ship = std::make_unique<Ship>();
 
-    for (const auto& obj : objects) {
-        JsonSerializer::put("objects", obj);
-    }
+    JsonSerializer::put("ships", ship);
 
-    std::cout << "object blok elements number: " << JsonSerializer::getSize("objects") << '\n';
-
-    std::unique_ptr<ExampleObject> emptyObj{};
-    JsonSerializer::put("objects", emptyObj);
-
-    JsonSerializer::putValue("block 1", "name", 1);
-    JsonSerializer::putValue("block 1", "name", 2);
-    JsonSerializer::putValue("block 1", "name", 3);
-    JsonSerializer::putValue("block 1", "text", "description");
-
-    std::cout << "block 1 elements number: " << JsonSerializer::getSize("block 1") << '\n';
-
-    JsonSerializer::putValue("block 2", 1, "name", 1);
-    JsonSerializer::putValue("block 2", 2, "name", 2);
-    JsonSerializer::putValue("block 2", 1, "name", 3);
-    JsonSerializer::putValue("block 2", 5, "name", 5);
-
-    std::cout << "block 2 elements number: " << JsonSerializer::getSize("block 2") << '\n';
-
-    JsonSerializer::save("example.json");
-
+    JsonSerializer::save("Ship.json");
     JsonSerializer::clear();
 
-    JsonSerializer::load("example.json");
+    JsonSerializer::load("Ship.json");
 
-    std::cout << JsonSerializer::getValue<int>("block 1", "name").value() << '\n';
-    std::cout << JsonSerializer::getValue<std::string>("block 1", "text").value() << '\n';
-
-    std::cout << JsonSerializer::getValue<int>("block 2", 1, "name").value() << '\n';
-    std::cout << JsonSerializer::getValue<int>("block 2", 2, "name").value() << '\n';
-    std::cout << JsonSerializer::getValue<int>("block 2", 5, "name").value() << '\n';
-
-    auto object2 = JsonSerializer::get<std::unique_ptr<ExampleObject>>("objects", 5);
-    if (object2) {
-        object2.value()->print();
+    auto size = JsonSerializer::getSize("ships");
+    if (size) {
+        auto isShip = JsonSerializer::get<std::unique_ptr<Ship>>("ships", 0);
+        if (auto ship = std::move(isShip.value())) {
+            std::cout << "ship name = " << ship->name << '\n';
+        }
     }
-
-    object2 = JsonSerializer::get<std::unique_ptr<ExampleObject>>("objects", 1);
-    if (object2) {
-        object2.value()->print();
-    }
-
-    std::cout << "where: " << JsonSerializer::getSize("where") << '\n';
-    JsonSerializer::getValue<int>("block 1", "clue");
-    JsonSerializer::getValue<int>("block 2", 4, "name");
-    JsonSerializer::save("example2.json");
 
     return 0;
 }
