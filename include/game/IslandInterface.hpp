cb9274be0fb6af5c8e45f208b927ee3d1f4ee554
lukasz-kukulka <qqla83@gmail.com>
2021-10-21 19:11:37 +0100

    Clear interface

--- include/game/IslandInterface.hpp
+++ include/game/IslandInterface.hpp
@@ -2,21 +2,13 @@
 
 #include "Coordinates.hpp"
 
-#include <memory>
 #include <string>
 
 class IslandInterface {
 public:
-    IslandInterface(std::string& name, std::unique_ptr<Coordinates>& position, int level_);
     virtual ~IslandInterface();
 
-    std::string getName() const { return name_; }
-    Coordinates* getCoordinates() const { return position_.get(); }
-    int getLevel() const { return level_; }
-
-private:
-    std::string name_;
-    std::unique_ptr<Coordinates>position_;
-    int level_;
-
+    virtual std::string getName() = 0;
+    virtual Coordinates* getCoordinates() = 0;
+    virtual int getLevel() = 0;
 };
