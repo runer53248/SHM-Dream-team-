994113213291007e7f56a650a734a9b5a53fc6a2
lukasz-kukulka <qqla83@gmail.com>
2021-10-21 17:18:16 +0100

    IsladInterface get level

--- include/game/IslandInterface.hpp
+++ include/game/IslandInterface.hpp
@@ -7,12 +7,13 @@
 
 class IslandInterface {
 public:
-    IslandInterface(std::string& name, std::unique_ptr<Coordinates>& position);
+    IslandInterface(std::string& name, std::unique_ptr<Coordinates>& position, int level_);
     virtual ~IslandInterface();
     Coordinates* getCoordinates() const { return position_.get(); }
+    int getLevel() const { return level_; }
 
 private:
     std::string name_;
     std::unique_ptr<Coordinates>position_;
-    uint8_t level_;
+    int level_;
 };
